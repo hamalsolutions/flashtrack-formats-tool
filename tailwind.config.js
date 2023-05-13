e618a82fd81b97ebea1e9099a1f9eb8e9e4ca055
Pedro Labrador <25873364+PedroLabrador@users.noreply.github.com>
2023-05-12 21:09:40 -0400

    FT20-2208 add barcodes (#12)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        maxHeight: {
+          "80vh": "80vh",
+        }
+      },
     },
     plugins: [],
   }
