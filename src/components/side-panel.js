4c2e77b3c4e5e8b26e3cd389557ac678a751d259
fabiolayerim <fabiolayerim@hotmail.com>
2023-05-17 23:40:46 -0400

    FT20-2194 re add icon

--- src/components/side-panel.js
+++ src/components/side-panel.js
@@ -5,6 +5,7 @@ import {
   DeviceMobileIcon,
   ColorSwatchIcon,
   TagIcon,
+  ArrowsExpandIcon,
 } from '@heroicons/react/outline';
 import ColorPalette from './color-palette';
 import TextEditor from './text-editor';
