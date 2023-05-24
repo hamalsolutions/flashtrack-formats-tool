fc5c51c11aa3e0f92da114ea42eed988550fbaf7
hedryf <hedryf@gmail.com>
2023-05-23 20:44:55 -0400

    FT20_2231_Add_cm_Resize

--- src/components/sizelabel-editor.js
+++ src/components/sizelabel-editor.js
@@ -8,18 +8,60 @@ export default function SizeLabelEditor({
 }) {
   const [wLabel, setWlabel] = useState();
   const [hLabel, setHlabel] = useState();
+  const [metric, setMetric] = useState("in");
+  const [wVal, setWVal] = useState();
+  const [hVal, setHVal] = useState();
 
   const widthlabel = (e) => {
-    const val = e.target.value * 88.088012 + 'px';
+    let val;
+    if(metric==="in")
+    {
+     val = e.target.value * 88.088012 + 'px';
+    }
+    else
+    {
+      val = e.target.value * 37.795280352161 + 'px';
+    }
+    
     setWlabel(val);
+    setWVal(e.target.value);
     onChangeW(val);
   };
 
   const heightlabel = (e) => {
-    const val = e.target.value * 88.088012 + 'px';
+    let val;
+    if(metric==="in")
+    {
+       val = e.target.value * 88.088012 + 'px';
+    }
+    else
+    {
+      val = e.target.value * 37.795280352161 + 'px';
+    }
+    
     setHlabel(val);
-    onChangeH(val);
+    setHVal(e.target.value);
+    onChangeH(val); 
   };
+
+  const handleMetric = (event) => {
+    if(event.target.value==="in"){
+      setHlabel(hVal * 88.088012 + 'px');
+      onChangeH(hVal * 88.088012 + 'px'); 
+      setWlabel(wVal * 88.088012 + 'px');
+      onChangeW(wVal * 88.088012 + 'px');
+    }
+    else
+    {
+      setHlabel(hVal * 37.795280352161 + 'px');
+      onChangeH(hVal * 37.795280352161 + 'px'); 
+      setWlabel(wVal * 37.795280352161 + 'px');
+      onChangeW(wVal * 37.795280352161 + 'px');
+    }
+    setMetric(event.target.value);
+    
+  };
+
 
   return (
     <>
@@ -33,7 +75,6 @@ export default function SizeLabelEditor({
             placeholder="width"
             onChange={widthlabel}
           />
-          <span className="ml-2">In</span>
         </div>
         <br></br>
         <div className="relative flex">
@@ -46,6 +87,13 @@ export default function SizeLabelEditor({
             onChange={heightlabel}
           />
         </div>
+        <br></br>
+        <span >
+            <select onChange={handleMetric}  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400   dark:border-gray-300 dark:placeholder-gray-400 dark:text-gray-800">
+              <option key="0" value="in">In</option>
+              <option key="1" value="cm">cm</option>
+            </select>
+          </span>
       </div>
     </>
   );
